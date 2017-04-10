aaa81c3e3c7b6786f94de9c925f5d783223b191b
azm0043 <andrew.muscha@auburn.edu>
2017-04-10 15:10:15 -0500

    Reorganized and added .gitignore

--- NasaFrontEndHWFilter/MDK-ARM/NASAFrontEndHWFilter/startup_stm32f446xx.d
+++
@@ -1,1 +0,0 @@
-nasafrontendhwfilter\startup_stm32f446xx.o: startup_stm32f446xx.s
--- NasaFrontEndHWFilter/MDK-ARM/NASAFrontEndHWFilter/usart_utilities.d
+++
@@ -1,2 +0,0 @@
-nasafrontendhwfilter\usart_utilities.o: ..\Src\stm32_hal_utilities-master\usart\usart_utilities.c
-nasafrontendhwfilter\usart_utilities.o: ..\Src\stm32_hal_utilities-master\usart\usart_utilities.h
