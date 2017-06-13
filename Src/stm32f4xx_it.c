3a84b0d1db15b7cf824b6b98066d263d55c3f5f4
azm0043 <andrew.muscha@auburn.edu>
2017-06-13 13:40:27 -0500

    Added in timer for serial sampling, added support for start flag check

--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -148,7 +148,7 @@ void TIM6_DAC_IRQHandler(void)
   /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
 
   /* USER CODE END TIM6_DAC_IRQn 0 */
-  //HAL_DAC_IRQHandler(&hdac);
+  HAL_DAC_IRQHandler(&hdac);
   HAL_TIM_IRQHandler(&htim6);
   /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
 
