287fc8de962986acb0fc039717b83b65f3e40532
Markus <mark.kreitzer@gmail.com>
2017-12-11 14:33:29 -0600

    Updated with moving average filter.

--- GUI/TempDisplay.py
+++ GUI/TempDisplay.py
@@ -9,7 +9,7 @@ import serial
 
 from GUI import settings
 
-temperature = "asdf"
+temperature = "NA"
 top = Tk()
 top.geometry("600x600")
 top.title("Sensor Temperature")
@@ -19,13 +19,16 @@ myLabel.pack()
 
 class Looping(object):
     def __init__(self):
-        #my_font = tkFont.Font(family='Helvetica', size=100, weight='bold')
+        # my_font = tkFont.Font(family='Helvetica', size=100, weight='bold')
         self.B_start = Tkinter.Button(top, text="Start", command=self.button_start)
         self.B_start.pack(fill=BOTH, expand=0)
         self.B_stop = Tkinter.Button(top, text="Stop", command=self.button_stop)
         self.B_stop.pack(fill=BOTH, expand=0)
         self.isRunning = True
         self.ser = serial.Serial(settings.serial_port, 9600, timeout=1)
+        self.temperature = None
+        self.alpha = 0.5
+
 
     def isConnected(self):
         return self.ser.is_open()
@@ -36,17 +39,35 @@ class Looping(object):
         t = threading.Thread(target=l.get_temperature)
         t.start()
 
+    def ma(self, new_temp):
+        ma_temp = self.alpha * new_temp + (1 - self.alpha) * self.temperature
+        print ma_temp
+        return ma_temp
+
+    def convert_temp(self, temperature_int):
+        print "temp_int", temperature_int
+        temp_c = (3.3 * temperature_int / 255.0) / 0.01
+        print temp_c
+        return temp_c
+
     def get_temperature(self):
         global temperature
         while self.isConnected:
             temperature = self.ser.readline()
             try:
                 temperature = int(temperature.strip())
-                temperature = (3.3 * temperature/255.0) / 0.01
-                myLabel.config(text = "%0.2f deg C" % temperature)
+                temperature = self.convert_temp(temperature)
+
+                # Added this to intialize the average temperature close to the value its reporting so that it will
+                # take less time to stablilize.
+                if not self.temperature:
+                    self.temperature = temperature
+                temperature = self.ma(temperature)
+                self.temperature = temperature
+                myLabel.config(text="%0.2f deg C" % temperature)
             except ValueError as e:
                 pass
-                #print ">>"+temperature+"<<", e
+                # print ">>"+temperature+"<<", e
         else:
             print "Not connected to the communications system."
 
